cb92545902d7bfd936205b73ae0e4716df13248d
Sergio Flor <sflor5@alumno.uned.es>
2021-01-02 09:48:17 +0100

    Update quicksort unedstyle

--- src/algorithms/divideAndConquer/Quicksort.java
+++ src/algorithms/divideAndConquer/Quicksort.java
@@ -3,19 +3,39 @@ package algorithms.divideAndConquer;
 public class Quicksort {
 
     public static void sort(int[] input){
-        if (input.length != 0) quicksort(input, 0, input.length - 1);
+        if (input.length != 0) quicksortHoare(input, 0, input.length - 1);
     }
 
-    private static void quicksort(int[] input, int lo, int hi) {
+    private static void quicksortHoare(int[] input, int lo, int hi) {
         int p;
         if (lo < hi) {
-            p = partition(input, lo , hi);
-            quicksort(input, lo, p - 1);
-            quicksort(input, p + 1, hi);
+            p = partitionHoare(input, lo , hi);
+            quicksortHoare(input, lo, p);
+            quicksortHoare(input, p + 1, hi);
         }
     }
 
-    private static int partition(int[] T, int i, int j) {
+    private static int partitionHoare(int[] input, int lo, int hi) {
+        int pivot = input[(hi + lo) / 2];
+        int i = lo - 1, j = hi + 1;
+        while (true) {
+            do i++; while (input[i] < pivot);
+            do j--; while (input[j] > pivot);
+            if (i >= j) return j;
+            swap(input, i, j);
+        }
+    }
+
+    private static void quicksortUnedStyle(int[] input, int lo, int hi) {
+        int p;
+        if (lo < hi) {
+            p = partitionUnedStyle(input, lo , hi);
+            quicksortUnedStyle(input, lo, p - 1);
+            quicksortUnedStyle(input, p + 1, hi);
+        }
+    }
+
+    private static int partitionUnedStyle(int[] T, int i, int j) {
         int p = T[i];
         int k = i, l = j + 1;
         do k++; while (T[k] <= p && k < j);
