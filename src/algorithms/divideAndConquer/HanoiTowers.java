13ed03655570502f98e11f9289080fc07d1edb7b
Sergio Flor <sflor5@alumno.uned.es>
2021-01-19 20:14:25 +0100

    Hanoi Towers -> prints format fixed

--- src/algorithms/divideAndConquer/HanoiTowers.java
+++ src/algorithms/divideAndConquer/HanoiTowers.java
@@ -15,7 +15,9 @@ public class HanoiTowers {
       }
    }
 
-   private static final int RINGS = 4;
+   //Set number of rings here
+   private static final int RINGS = 6;
+
    private static Stackie
            A = new Stackie(new Stack<Integer>(), 'A'),
            B = new Stackie(new Stack<Integer>(), 'B'),
@@ -24,28 +26,28 @@ public class HanoiTowers {
    public static void main(String[] args) {
       IntStream.range(0, RINGS).map(n -> RINGS - n).forEach(n -> A.stack.push(n));
       printStacks();
-      hanoi(0, A, B, C, RINGS);
+      hanoiSolver(0, A, B, C, RINGS);
       printStacks();
    }
 
-   private static void hanoi(int call, Stackie from, Stackie to, Stackie other, int n) {
+   private static void hanoiSolver(int call, Stackie from, Stackie to, Stackie other, int n) {
       if (n==0) return;
-      hanoi(1, from, other, to, n-1);
+      hanoiSolver(1, from, other, to, n-1);
       to.stack.push(from.stack.pop());
-      System.out.format("\n\tMove %c top to %c\n\n", from.letter, to.letter);
-      hanoi(2, other, to, from, n-1);
+      System.out.format("\tMove %c top to %c\n", from.letter, to.letter);
+      hanoiSolver(2, other, to, from, n-1);
    }
 
    private static void printStacks() {
       System.out.println("-----------------------");
       System.out.print("Stack A: ");
-      A.stack.forEach(n -> System.out.print(n+" "));
+      A.stack.forEach(n -> System.out.format("%d ",n));
       System.out.println();
       System.out.print("Stack B: ");
-      B.stack.forEach(n -> System.out.print(n+" "));
+      B.stack.forEach(n -> System.out.format("%d ",n));
       System.out.println();
       System.out.print("Stack C: ");
-      C.stack.forEach(n -> System.out.print(n+" "));
+      C.stack.forEach(n -> System.out.format("%d ",n));
       System.out.println();
       System.out.println("-----------------------");
    }
