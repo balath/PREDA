aa7f3eb5e1c383a7816825a807db6628e31e5e86
Sergio Flor <sflor5@alumno.uned.es>
2021-01-26 19:15:02 +0100

    Quicksort updated

--- src/algorithms/divideAndConquer/Quicksort.java
+++ src/algorithms/divideAndConquer/Quicksort.java
@@ -3,9 +3,15 @@ package algorithms.divideAndConquer;
 public class Quicksort {
 
     public static void sort(int[] input){
-        if (input.length != 0) quicksortHoare(input, 0, input.length - 1);
+        if (input.length != 0) {
+            //quicksortHoare(input, 0, input.length - 1);
+            quicksortUnedStyle(input, 0, input.length - 1);
+        }
     }
 
+    /**
+     * Algoritmo Quicksort de Hoare (https://en.wikipedia.org/wiki/Quicksort)
+     */
     private static void quicksortHoare(int[] input, int lo, int hi) {
         int p;
         if (lo < hi) {
@@ -14,7 +20,6 @@ public class Quicksort {
             quicksortHoare(input, p + 1, hi);
         }
     }
-
     private static int partitionHoare(int[] input, int lo, int hi) {
         int pivot = input[(hi + lo) / 2];
         int i = lo - 1, j = hi + 1;
@@ -26,32 +31,36 @@ public class Quicksort {
         }
     }
 
-    private static void quicksortUnedStyle(int[] input, int lo, int hi) {
+
+    /**
+     * Algoritmo Quicksort del Libro base (ed. 2011)
+     */
+    private static void quicksortUnedStyle(int[] input, int leftIndex, int rightIndex) {
         int p;
-        if (lo < hi) {
-            p = partitionUnedStyle(input, lo , hi);
-            quicksortUnedStyle(input, lo, p - 1);
-            quicksortUnedStyle(input, p + 1, hi);
+        if (leftIndex < rightIndex) {
+            p = partitionUnedStyle(input, leftIndex , rightIndex);
+            quicksortUnedStyle(input, leftIndex, p - 1);
+            quicksortUnedStyle(input, p + 1, rightIndex);
         }
     }
-
-    private static int partitionUnedStyle(int[] T, int i, int j) {
-        int p = T[i];
-        int k = i, l = j + 1;
-        do k++; while (T[k] <= p && k < j);
-        do l--; while(T[l] > p);
-        while (k < l) {
-            swap(T, k, l);
-            do k++; while (T[k] <= p);
-            do l--; while (T[l] > p);
+    private static int partitionUnedStyle(int[] vector, int leftIndex, int rightIndex) {
+        int pivot = vector[leftIndex];
+        int leftPos = leftIndex, rightPos = rightIndex + 1;
+        do leftPos++; while (vector[leftPos] <= pivot && leftPos < rightIndex);
+        do rightPos--; while(vector[rightPos] > pivot);
+        while (leftPos < rightPos) {
+            swap(vector, leftPos, rightPos);
+            do leftPos++; while (vector[leftPos] <= pivot);
+            do rightPos--; while (vector[rightPos] > pivot);
         }
-        swap(T, i, l);
-        return l;
+        swap(vector, leftIndex, rightPos);
+        return rightPos;
     }
 
-    private static void swap(int[] input, int k, int l) {
-        int swap = input[k];
-        input[k] = input[l];
-        input[l] = swap;
+    /* Método común que intercambia las posiciones i y j de un vector */
+    private static void swap(int[] vector, int i, int j) {
+        int swap = vector[i];
+        vector[i] = vector[j];
+        vector[j] = swap;
     }
 }
